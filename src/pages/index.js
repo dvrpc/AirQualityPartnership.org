a277a537faa2489a334283103caafd2b1057c5a2
hachadorian <hachadorian98@gmail.com>
2024-05-15 14:28:37 -0400

    fixed tracking

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,27 @@ import Forecast from "../components/Forecast";
 import Layout from "../components/Layout";
 import "../css/index.css";
 import Seo from "../components/Seo";
+import { Helmet } from "react-helmet";
 
 const IndexPage = () => {
   return (
-    <Layout>
-      <Seo title="Home" />
-      <Forecast />
-    </Layout>
+    <>
+      <Helmet>
+        <script type="text/javascript">
+          {`function onLoad() {
+              cntrUpTag.track("cntrData", "7490abf38e56e9f8");
+          }`}
+        </script>
+        <script
+          src="https://cdn01.basis.net/assets/up.js"
+          onload="onLoad()"
+        ></script>
+      </Helmet>
+      <Layout>
+        <Seo title="Home" />
+        <Forecast />
+      </Layout>
+    </>
   );
 };
 
